c7a752b309f8985cc9d8dac163b62da890bcb8e7
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-03-16 19:33:29 +0100

    Update TrollScript.lua

--- TrollScript.lua
+++ TrollScript.lua
@@ -1,1 +1,19 @@
-loadstring(game:HttpGet("https://pastebin.com/aLp8k5yH"))()
+local char = game.Players.LocalPlayer.Character
+local hrp = char:WaitForChild("HumanoidRootPart")
+local ts = game:GetService("TweenService")
+local randball2
+local rot = 0
+local tweentime = 1/30
+
+while task.wait(tweentime) do
+    local balls = game.workspace:WaitForChild("Balls"):GetChildren()
+    local randball = balls[math.random(1,#balls)]
+    if randball then
+        local rad = math.rad
+        local r = rad(rot)
+ 		local newcframe = randball.CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,-15)
+        rot+=90
+
+        ts:Create(hrp, TweenInfo.new(tweentime), {CFrame = newcframe}):Play()
+    end
+end
