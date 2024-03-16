10749d849b3b6c15d9945b57fa28eb3bc66a3b1b
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-03-16 20:00:46 +0100

    Update TrollScript.lua

--- TrollScript.lua
+++ TrollScript.lua
@@ -11,7 +11,7 @@ while task.wait(tweentime) do
     if randball then
         local rad = math.rad
         local r = rad(rot)
- 		local newcframe = randball.CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,-10)
+ 		local newcframe = randball.CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,-15)
         rot+=math.random(15,180)
 
         ts:Create(hrp, TweenInfo.new(tweentime), {CFrame = newcframe}):Play()
