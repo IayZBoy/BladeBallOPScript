d7e196c88ff0831d4eb10bc7c0c2c32eb0458414
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-03-17 12:03:28 +0100

    Update TrollScriptV2.lua

--- TrollScriptV2.lua
+++ TrollScriptV2.lua
@@ -16,6 +16,8 @@ function start()
                 local r = rad(rot)
                 local newcframe = randball.CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,-15)
                 rot+=90
+
+                hrp.Velocity=Vector3.Zero
                 ts:Create(hrp, TweenInfo.new(tweentime/2), {CFrame = newcframe}):Play()
             end
         end
