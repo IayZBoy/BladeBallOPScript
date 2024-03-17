2a55f792d772c2871e425ec689d1adcaac816231
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-03-17 03:42:16 +0100

    Update TrollScript.lua

--- TrollScript.lua
+++ TrollScript.lua
@@ -5,22 +5,18 @@ local rot = 0
 local tweentime = 1/45
 
 function start()
-while task.wait(tweentime) do
-    pcall(function()
-        local balls = game.workspace:WaitForChild("Balls"):GetChildren()
-        pcall(function()
-            local randball = balls[math.random(1,#balls)]
-            if randball then
-                local rad = math.rad
-                local r = rad(rot)
-                local newcframe = randball.CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,-7.5)
-                rot+=math.random(15,180)
-                hrp.Velocity=Vector3.zero
-                ts:Create(hrp, TweenInfo.new(tweentime), {CFrame = newcframe}):Play()
-            end
-        end)
-    end)
-end
+    while task.wait(tweentime) do
+          local balls = game.workspace:WaitForChild("Balls"):GetChildren()
+          local randball = balls[math.random(1,#balls)]
+          if randball then
+              local rad = math.rad
+              local r = rad(rot)
+              local newcframe = randball.CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,-7.5)
+              rot+=math.random(15,180)
+              hrp.Velocity=Vector3.zero
+              ts:Create(hrp, TweenInfo.new(tweentime), {CFrame = newcframe}):Play()
+        end
+    end
 end
 start()
 game.Workspace:WaitForChild("Balls").ChildAdded:Connect(start)
