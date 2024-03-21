b09192924f39fd23a42199b94af248014328f809
IayZBoy <114676300+IayZBoy@users.noreply.github.com>
2024-03-21 09:03:41 +0100

    e

--- TrollScript.lua
+++ TrollScript.lua
@@ -15,7 +15,6 @@ local randball
 local rot = 0
 local rad = math.rad
 local clamp = math.clamp
-local SpamEnabled = false
 local tweentime = game:GetService("RunService").Heartbeat:Wait()
 local Info = TweenInfo.new(tweentime/4.5, Enum.EasingStyle.Sine, Enum.EasingDirection.InOut)
 local NEVERLOSE = loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/NEVERLOSE-UI-Nightly/main/source.lua"))()
@@ -23,85 +22,106 @@ local NEVERLOSE = loadstring(game:HttpGet("https://raw.githubusercontent.com/334
 NEVERLOSE:Theme("Dark")
 
 local data = {
-	AutoParryEnabled = true,
-	VisualiserEnabled = false,
-	ParryTime=0.7,
-	TrollEnabled = false,
-	TrollDistanceFactor = 0.2
+	Combat = {
+		AutoParryEnabled = false,
+		VisualiserEnabled = false,
+		SpamEnabled=false,
+		ParryTime=0.7
+	},
+	Trolls = (
+		TrollEnabled = false,
+		TrollDistanceFactor = 0.2,
+		Gravity=196.2
+		FollowPlayer = false,
+		PlayerToFollow = nil
+	),
+	Player = {
+		WalkSpeed = 36,
+		JumpPower = 50
+	}
 }
+function StartScript()
+	local window = NEVERLOSE:AddWindow("ZBOY HUB", "BLADE BALL - NEXT GENERATION")
 
-local window = NEVERLOSE:AddWindow("ZBOY HUB", "BLADE BALL - NEXT GENERATION")
+	local Main = window:AddTab("Main","earth")
+	local Creds = window:AddTab("Credits", "list")
 
-local Main = window:AddTab("Main","earth")
-local Creds = window:AddTab("Credits", "list")
+	local Combat = Main:AddSection("COMBAT", "left")
+	local Trolls = Main:AddSection("TROLLS", "right")
+	local Player = Main:AddSection("PLAYER", "left")
+	local PlayerList = Trolls:AddDropdown("Players", game.Players:GetPlayers(),game.Players.LocalPlayer, function(v)
+		data.Trolls.PlayerToFollow=v.Name
+	end)
 
-local Combat = Main:AddSection("COMBAT", "left")
-local Trolls = Main:AddSection("TROLLS", "right")
-local Player = Main:AddSection("PLAYER", "left")
---local PlayerList = Trolls:AddDropdown("Players", game.Players:GetPlayers(),game.Players.LocalPlayer)
+	Combat:AddToggle("Auto Parry",true,function(val)
+		data.Combat.AutoParryEnabled=val
+	end)
+	Combat:AddToggle("Visualiser",false,function(val)
+		data.Combat.VisualiserEnabled=val
+	end)
+	Combat:AddSlider("Parry Time",0,100,60,function(val)
+		data.Combat.ParryTime=val/100
+	end)
+	Combat:AddToggle("Spam",false,function(val)
+		data.Combat.SpamEnabled=val
+	end)
 
-Combat:AddToggle("Auto Parry",true,function(val)
-	data.AutoParryEnabled=val
-end)
-Combat:AddToggle("Visualiser",false,function(val)
-	data.VisualiserEnabled=val
-end)
-Combat:AddSlider("Parry Time",0,100,60,function(val)
-	data.ParryTime=val/100
-end)
+	Trolls:AddToggle("Troll Enabled",false,function(val)
+		data.TrollEnabled=val
+	end)
 
-Trolls:AddToggle("Troll Enabled",false,function(val)
-	data.TrollEnabled=val
-end)
+	Trolls:AddToggle("Follow Player",false,function(a)
+		data.Trolls.FollowPlayer=a
+	end)
 
---[[Trolls:AddToggle("Follow Player",false,function(a)
-	print(a)
-end)]]
+	Trolls:AddButton("Update Players",function(a)
+		PlayerList:Refresh()
+	end)
 
---[[Trolls:AddButton("Update Players",function(a)
-	PlayerList:Refresh()
-	print("Updated Player List")
-end)]]
+	Trolls:AddSlider("Low Gravity",0,300,196.2,function(a)
+		if hum then
+			hum.UseJumpPower=true
+		end
+		data.Trolls.Gravity=a
+		end
+	end)
 
-Trolls:AddToggle("Low Gravity",false,function(a)
-	if hum then
-		hum.UseJumpPower=true
-	end
-	if a then
-		workspace.Gravity=50
+	Trolls:AddSlider("Troll Distance Factor",0,100,20,function(distance)
+		data.TrollDistanceFactor=distance/100
+	end)
+
+	Player:AddSlider("Speed",36,250,36,function(a)
+		if hum then
+			hum.WalkSpeed=a
+		end
+	end)
+	Player:AddSlider("JumpPower",50,250,50,function(a)
+		if hum then
+			hum.UseJumpPower=true
+			hum.JumpPower=a
+		end
+	end)
+
+	local catsus = Creds:AddSection("3345-c-a-t-s-u-s")
+	catsus:AddLabel("CREDITS TO")
+	catsus:AddLabel("3345-c-a-t-s-u-s")
+	catsus:AddLabel("FOR THE IDEA AND THE UI")
+end
+
+local KeySystem = NEVERLOSE:KeySystem("Key System","!QAZ1qaz@WSX2wsx", function(Key)
+	if Key=="!QAZ1qaz@WSX2wsx" then
+		return true
 	else
-		workspace.Gravity=196.2
+		return false
 	end
-end)
-
-Trolls:AddSlider("Troll Distance Factor",0,100,20,function(distance)
-	data.TrollDistanceFactor=distance/100
-end)
-
-Player:AddSlider("Speed",36,250,36,function(a)
-	if hum then
-		hum.WalkSpeed=a
-	end
-end)
-Player:AddSlider("JumpPower",50,250,50,function(a)
-	if hum then
-		hum.UseJumpPower=true
-		hum.JumpPower=a
-	end
-end)
-
-local CredsSec = Creds:AddSection("CREDITS")
-
-CredsSec:AddLabel("CREDITS TO")
-CredsSec:AddLabel("3345-c-a-t-s-u-s")
-CredsSec:AddLabel("FOR THE IDEA")
-CredsSec:AddLabel("AND THE UI")
+end
+KeySystem:CallBack(StartScript)
 
 local indicatorPart = Instance.new("Part")
 indicatorPart.Size = Vector3.new(ballspeed, ballspeed, ballspeed)
 indicatorPart.Anchored = true
 indicatorPart.CanCollide = false
-indicatorPart.Transparency = 0
+indicatorPart.Transparency = 1
 indicatorPart.Color = Color3.fromRGB(255,255,255)
 indicatorPart.Material = Enum.Material.ForceField
 indicatorPart.Shape=Enum.PartType.Ball
@@ -119,10 +139,14 @@ function GetPoint()
 	return Camera:WorldToScreenPoint(hrp.Position)
 end
 
+function GetDistance()
+	return GetDistance()
+end
+
 function UpdateIndicator()
     if randball:GetAttribute("target")==plr.Name and randball:GetAttribute("realBall") then
         indicatorPart.Color=Color3.fromRGB(255,125,125)
-    elseif plrballdist<=ballspeed*data.ParryTime and randball:GetAttribute("target")==plr.Name then
+    elseif GetDistance()<=ballspeed*data.ParryTime and randball:GetAttribute("target")==plr.Name then
         indicatorPart.Color=Color3.fromRGB(125,255,125)
     else
         indicatorPart.Color=Color3.fromRGB(255,255,255)
@@ -138,19 +162,22 @@ function UpdateIndicator()
     indicatorPart.Size=GetLocalSize()
 end
 
+function Parry()
+	hit:FireServer(data.ParryTime, CFrame.new(),{},{GetPoint().X,GetPoint().Y})
+end
+
 function TryParry()
-     if plrballdist*(1+plr:GetNetworkPing())<=ballspeed*0.data.ParryTime and randball:GetAttribute("target")==plr.Name and data.AutoParryEnabled then
-         local point = GetPoint()
-         hit:FireServer(0.data.ParryTime, CFrame.new(),{},{point.X,point.Y})
+     if GetDistance()<=ballspeed*data.ParryTime and randball:GetAttribute("target")==plr.Name and data.AutoParryEnabled then
+         Parry()
+	 elseif GetDistance()<=ballspeed*data.ParryTime and data.SpamEnabled then
+		Parry()
      end
 end
 
 function LaunchItems()
 	TryParry()
 	UpdateIndicator()
-	if SpamEnabled then
-		TryParry()
-	end
+	workspace.Gravity=data.Trolls.Gravity
 end
 
 game:GetService("RunService").RenderStepped:Connect(LaunchItems)
@@ -178,9 +205,12 @@ function start()
                 ballspeed = clamp(randball.Velocity.Magnitude,6,math.huge)
                 rot=math.random(-180,180)
                 
-                if data.TrollEnabled then
+                if data.Trolls.TrollEnabled then
                     hrp.AssemblyLinearVelocity=Vector3.zero
                     ts:Create(hrp, Info, {CFrame = newcframe}):Play()
+				elseif data.Trolls.FollowPlayer then
+					hrp.AssemblyLinearVelocity=Vector3.zero
+					ts:Createhrp, Info, {CFrame = workspace:FindFirstChild(data.PlayerToFollow):FindFirstChild("HumanoidRootPart").CFrame*CFrame.Angles(0,r,0)*CFrame.new(0,0,10)}
                 end
             end
         end
